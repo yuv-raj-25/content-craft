b3ca4268c99b579896b4c6e824d6bb1b2583ce7e
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2025-01-15 19:46:03 +0530

    feat: webhooks are done & middleware updated

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,16 +5,16 @@ declare type CreateUserParams = {
     clerkId: string;
     email: string;
     username: string;
-    firstName: string;
-    lastName: string;
-    photo: string;
+    firstName: string | null;
+    lastName: string | null;
+    photo: string | null;
   };
   
   declare type UpdateUserParams = {
-    firstName: string;
-    lastName: string;
     username: string;
-    photo: string;
+    firstName: string | null;
+    lastName: string | null;
+    photo: string | null;
   };
   
   // ====== IMAGE PARAMS
