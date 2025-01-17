5358806f2ea0a5a0f13aa701e74ea1ebb1b541bc
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2025-01-17 16:56:21 +0530

    feat: sync of data clerk to mongodb done

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,39 +1,39 @@
-import Header from '@/components/shared/Header'
-import TransformationForm from '@/components/shared/TransformationForm';
-import { transformationTypes } from '@/constants'
-import { getUserById } from '@/lib/actions/user.actions';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-import { NextResponse } from 'next/server';
+// import Header from '@/components/shared/Header'
+// import TransformationForm from '@/components/shared/TransformationForm';
+// import { transformationTypes } from '@/constants'
+// import { getUserById } from '@/lib/actions/user.actions';
+// import { auth } from '@clerk/nextjs/server';
+// import { redirect } from 'next/navigation';
+// import { NextResponse } from 'next/server';
 
-const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
-  const {userId} = await  auth();
-  if(!userId){
-    return NextResponse.json({error: "Unauthorized"}, {status: 401});
-}
-  const transformation = transformationTypes[type];
+// const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+//   const {userId} = await  auth();
+//   if(!userId){
+//     return NextResponse.json({error: "Unauthorized"}, {status: 401});
+// }
+//   const transformation = transformationTypes[type];
 
-  if(!userId) redirect('/sign-in')
+//   if(!userId) redirect('/sign-in')
 
-  const user = await getUserById(userId);
+//   const user = await getUserById(userId);
 
-  return (
-    <>
-      <Header 
-        title={transformation.title}
-        subtitle={transformation.subTitle}
-      />
+//   return (
+//     <>
+//       <Header 
+//         title={transformation.title}
+//         subtitle={transformation.subTitle}
+//       />
     
-      <section className="mt-10">
-        <TransformationForm 
-          action="Add"
-          userId={user._id}
-          type={transformation.type as TransformationTypeKey}
-          creditBalance={user.creditBalance}
-        />
-      </section>
-    </>
-  )
-}
+//       <section className="mt-10">
+//         <TransformationForm 
+//           action="Add"
+//           userId={user._id}
+//           type={transformation.type as TransformationTypeKey}
+//           creditBalance={user.creditBalance}
+//         />
+//       </section>
+//     </>
+//   )
+// }
 
-export default AddTransformationTypePage
+// export default AddTransformationTypePage
