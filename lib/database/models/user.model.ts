5e51ffbb27e37ee2d16442c56f9e9728db953535
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2025-01-17 17:09:26 +0530

    feat: sync of data clerk to mongodb done

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,15 +1,4 @@
-import { Schema, model, models, Document } from "mongoose";
-
-export interface User extends Document {
-    clerkId: string; // Unique identifier for the user from Clerk
-    email: string;   // Email address of the user
-    username: string; // Unique username for the user
-    photo: string;   // URL to the user's photo
-    firstName?: string; // Optional first name of the user
-    lastName?: string;  // Optional last name of the user
-    planId?: number;    // Plan ID, defaults to 1 if not provided
-    creditBalance?: number; // Credit balance, defaults to 10 if not provided
-  }
+import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
   clerkId: {
@@ -21,12 +10,6 @@ const UserSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    validate: {
-      validator: (email: string) =>
-        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email),
-      message: (props: { value: string }) =>
-        `${props.value} is not a valid email!`,
-    },
   },
   username: {
     type: String,
